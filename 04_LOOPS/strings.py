1b72d654567f1e89181fbf215c91534b43d6214f
Shruthi KV <shruthikv@Shruthis-MacBook-Air.local>
2026-07-05 16:09:33 +0530

    strings

---
+++ 04_LOOPS/strings.py
@@ -0,0 +1,37 @@
+# # # name = 'Shru' \
+# # # 'thi'
+
+# # # print(name)
+
+# # name = 'Shruthi'
+# # print(name[0])
+# # # print(name[7])# out of range error
+# # print(name[-1])
+# # print(name[-7])
+# # # print(name[-8]) # out of range error
+
+# # text = 'Python'
+# # print(text[5])
+# # print(text[3:-1])
+# # print(text[0:2])
+
+
+
+
+# text = 'Shruthi01234567'
+
+# print(text[0:10:2])
+# print(text[:3])
+# print(text[0:])
+
+
+name = 'shruthi learning python'
+# name[0] = '9' # not possible
+
+print(len(name))
+
+# print(name.upper())
+# print(name.lower())
+
+# print(name.capitalize())
+# print(name.title())
